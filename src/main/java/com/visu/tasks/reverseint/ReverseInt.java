b2f57fabc476bf910967b3665eb56d87f2cacfd8
ViktorSuharev <viktor.suhareff@yandex.ru>
2017-08-28 23:42:58 +0300

    Rever Int init commit

---
+++ src/main/java/com/visu/tasks/reverseint/ReverseInt.java
@@ -0,0 +1,7 @@
+package com.visu.tasks.reverseint;
+
+/**
+ * Created by Viktor Sukharev on 28.08.2017.
+ */
+public class ReverseInt {
+}
---
+++ src/main/java/com/visu/tasks/wordpermutation/WordPermutation.java
@@ -0,0 +1,7 @@
+package com.visu.tasks.wordperm;
+
+/**
+ * Created by Viktor Sukharev on 27.08.2017.
+ */
+public class WordPerm {
+}
