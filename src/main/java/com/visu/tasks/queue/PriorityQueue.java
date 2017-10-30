381318909c5109a05b8df4942ebe39286b353f47
ViktorSuharev <viktor.suhareff@yandex.ru>
2017-10-31 00:15:25 +0300

    queue with priority

---
+++ src/main/java/com/visu/tasks/queue/PriorityQueue.java
@@ -0,0 +1,8 @@
+package com.visu.tasks.queue;
+
+public interface PriorityQueue<T> {
+
+    void push(Element element);
+
+    Element pop();
+}
---
+++ src/main/java/com/visu/tasks/queue/PriorityQueueImpl.java
@@ -0,0 +1,12 @@
+package com.visu.tasks.queue;
+
+public class PriorityQueueImpl<T> implements PriorityQueue<T> {
+
+    public void push(Element element) {
+
+    }
+
+    public Element pop() {
+        return null;
+    }
+}
